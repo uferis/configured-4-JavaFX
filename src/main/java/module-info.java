b061db7aab22fe80719b9a856b0bb8b1697ff2b7
Emka <dumbliauskasemilis@gmail.com>
2026-05-19 15:26:15 +0300

    Working Code

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ToDoAppUsingJavaFXEmilis {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens org.example to javafx.fxml;
--- src/main/java/org/example/controllers/Controller.java
+++ src/main/java/org/example/controllers/Controller.java
@@ -1,6 +1,47 @@
 package org.example.controllers;
 
-public class Controller {
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
+import org.example.models.TaskModel;
 
-    
+public class Controller {
+    @FXML public TextField taskTitle;
+    @FXML public TextField dueDate;
+    @FXML public ChoiceBox<String> priority;
+
+    @FXML public Button addBtn;
+
+    @FXML
+    public TableView<TaskModel> todoTable;
+    @FXML public TableColumn<TaskModel, String> tableTask;
+    @FXML public TableColumn<TaskModel, String> tableDueDate;
+    @FXML public TableColumn<TaskModel, String> tablePriority;
+
+    @FXML
+    public void initialize() { // the thing that will run first
+        priority.getItems().addAll("Low", "Medium", "High");
+        tableTask.setCellValueFactory(new PropertyValueFactory<>("task"));
+        tableDueDate.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+        tablePriority.setCellValueFactory(new PropertyValueFactory<>("priority"));
+
+        todoTable.setItems(tasks);
+    }
+
+    private ObservableList<TaskModel> tasks = FXCollections.observableArrayList();
+
+    public void addTask(){
+        tasks.add(new TaskModel(taskTitle.getText(), dueDate.getText(), priority.getValue()));
+
+        taskTitle.clear();
+        dueDate.clear();
+        priority.getSelectionModel();
+    }
+
+
+
 }
