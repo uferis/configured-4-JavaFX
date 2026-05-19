9ef1a0dc646543d11296af3d329ef2077517e991
Emka <dumbliauskasemilis@gmail.com>
2026-05-19 15:30:54 +0300

    Do not touch the configuration, unless you know what you're doing. It took me a long time -_- -Emka

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,14 +12,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("task-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
-        stage.setTitle("To-Do Application");
-        stage.setScene(scene);
-        stage.show();
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/task-view.fxml")); // why do i need a '/' ?????//??//?/
+        Scene scene = new Scene(fxmlLoader.load(), 700, 700); // Size of the screen
+        stage.setTitle("To-Do Application"); // Title of the window
+        stage.setScene(scene); // Which scene to show
+        stage.show(); // Shows
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(); // Launces the program
     }
 }
--- src/main/java/org/example/controllers/Controller.java
+++ src/main/java/org/example/controllers/Controller.java
@@ -1,15 +1,15 @@
 package org.example.controllers;
 
-import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.text.Text;
 import org.example.models.TaskModel;
 
 public class Controller {
+
+    // Delete or change <from here
     @FXML public TextField taskTitle;
     @FXML public TextField dueDate;
     @FXML public ChoiceBox<String> priority;
@@ -21,11 +21,12 @@ public class Controller {
     @FXML public TableColumn<TaskModel, String> tableTask;
     @FXML public TableColumn<TaskModel, String> tableDueDate;
     @FXML public TableColumn<TaskModel, String> tablePriority;
+    //to here>
 
     @FXML
     public void initialize() { // the thing that will run first
         priority.getItems().addAll("Low", "Medium", "High");
-        tableTask.setCellValueFactory(new PropertyValueFactory<>("task"));
+        tableTask.setCellValueFactory(new PropertyValueFactory<>("taskTitle"));
         tableDueDate.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
         tablePriority.setCellValueFactory(new PropertyValueFactory<>("priority"));
 
@@ -35,11 +36,13 @@ public class Controller {
     private ObservableList<TaskModel> tasks = FXCollections.observableArrayList();
 
     public void addTask(){
+        // adds to the observable list
         tasks.add(new TaskModel(taskTitle.getText(), dueDate.getText(), priority.getValue()));
 
+        // clears everything after
         taskTitle.clear();
         dueDate.clear();
-        priority.getSelectionModel();
+        priority.getSelectionModel().clearSelection(); // In the teacher's code .clearSelection() wasn't added
     }
 
 
